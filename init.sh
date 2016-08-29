3ed73a29e28dc42aacc672cbbe3d1e43d9c9d24c
kremy <kevanescence@hotmail.fr>
2016-08-29 22:26:09 +0200

    Infra: retrieve maildev host from apache server

--- init.sh
+++ init.sh
@@ -6,9 +6,12 @@ if [[ "$BUILD_TAG" == "" ]]; then
     ln -s "$TESTED_CODE" /var/www/html/ghostylink
     chmod -R 777 "$TESTED_CODE/tmp"
     chmod -R 777 "$TESTED_CODE/logs"
+    sed -i s/__tbr__maildev/"localhost"/ /etc/apache2/sites-available/000-default.conf
 else
     cp -R "$TESTED_CODE" /var/www/html/ghostylink
     chown -R www-data:www-data /var/www/html/ghostylink
+    sed -i s/__tbr__maildev/"maildev-$BUILD_TAG"/ /etc/apache2/sites-available/000-default.conf
 fi
 cp /image/apache-conf/default.conf /etc/apache2/sites-available/000-default.conf
+
 service apache2 start
