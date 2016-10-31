7cc2a83581735a0a1061fa7cf4fd65be8e79d712
kremy <kevanescence@hotmail.fr>
2016-10-31 11:49:17 +0100

    Init: use BUILD_TAG_SAFE variable if available

--- init.sh
+++ init.sh
@@ -2,15 +2,19 @@
 
 source /image/db_init.sh
 
-if [[ "$BUILD_TAG" == "" ]]; then
+if [[ "$BUILD_TAG_SAFE" == "" ]]; then
     ln -s "$TESTED_CODE" /var/www/html/ghostylink
     chmod -R 777 "$TESTED_CODE/tmp"
     chmod -R 777 "$TESTED_CODE/logs"
     sed -i s/__tbr__maildev/"localhost"/ /image/apache-conf/default.conf
 else
+    ## Legacy check for old ghostylink branches
+    if [[ "$BUILD_TAG_SAFE" == "" ]]; then
+        BUILD_TAG_SAFE="$BUILD_TAG"
+    fi
     cp -R "$TESTED_CODE" /var/www/html/ghostylink
     chown -R www-data:www-data /var/www/html/ghostylink
-    sed -i s/__tbr__maildev/"maildev-$BUILD_TAG"/ /image/apache-conf/default.conf
+    sed -i s/__tbr__maildev/"maildev-$BUILD_TAG_SAFE"/ /image/apache-conf/default.conf
 fi
 cp /image/commands/* /bin
 chmod -R 755 /bin
